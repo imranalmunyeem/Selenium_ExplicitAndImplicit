e072fc00c90cb2b03265fc3f0d3c1766c02b0975
IMRAN AL MUNYEEM <ialmunyeem@gmail.com>
2021-09-14 18:01:14 +0600

    Initial commit

---
+++ src/test/java/com/waittypes/selenium/WaitTypes.java
@@ -0,0 +1,29 @@
+package com.waittypes.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class WaitTypes {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://alaskatrips.poweredbygps.com/g/pt/hotels?MDPCID=ALASKA-US.TPS.BRAND.hotels.HOTEL");
+		driver.findElement(By.id("H-destination")).sendKeys("nyc");
+		driver.findElement(By.id("H-destination")).sendKeys(Keys.TAB);
+		driver.findElement(By.id("H-fromDate")).sendKeys(Keys.ENTER);
+
+		WebDriverWait d = new WebDriverWait(driver, 20);
+		d.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='resultsContainer']/section/article[1]")));
+//Thread.sleep(5000L);
+
+		driver.findElement(By.xpath("//div[@id='resultsContainer']/section/article[1]")).click();
+	}
+}
